f376c46502c81656fb89c6b938ca53cbea74c7fe
Angelo Geulin <angelogeulin123@gmail.com>
2023-03-31 15:20:57 +0800

    config: add Tailwindcss, FontAwesome, and Android integration to project

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,69 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
 
-import { IonicVue } from '@ionic/vue';
+import { IonicVue } from "@ionic/vue"
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/vue/css/core.css';
+import "@ionic/vue/css/core.css"
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/vue/css/normalize.css';
-import '@ionic/vue/css/structure.css';
-import '@ionic/vue/css/typography.css';
+import "@ionic/vue/css/normalize.css"
+import "@ionic/vue/css/structure.css"
+import "@ionic/vue/css/typography.css"
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/padding.css';
-import '@ionic/vue/css/float-elements.css';
-import '@ionic/vue/css/text-alignment.css';
-import '@ionic/vue/css/text-transformation.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/display.css';
+import "@ionic/vue/css/padding.css"
+import "@ionic/vue/css/float-elements.css"
+import "@ionic/vue/css/text-alignment.css"
+import "@ionic/vue/css/text-transformation.css"
+import "@ionic/vue/css/flex-utils.css"
+import "@ionic/vue/css/display.css"
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css"
+import "./theme/tailwind.css"
 
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-  
+/* add fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+
+/* add some free styles */
+import { faTwitter } from "@fortawesome/free-brands-svg-icons"
+import {
+  faUserSecret,
+  faPaw,
+  faCalendarDays,
+  faScissors,
+  faCircleInfo,
+  faLocationDot,
+  faPhone,
+  faEnvelope,
+  faSoap,
+  faSyringe,
+  faDog,
+  faCat,
+} from "@fortawesome/free-solid-svg-icons"
+
+library.add(
+  faTwitter,
+  faUserSecret,
+  faPaw,
+  faCalendarDays,
+  faScissors,
+  faCircleInfo,
+  faLocationDot,
+  faPhone,
+  faEnvelope,
+  faSoap,
+  faSyringe,
+  faDog,
+  faCat
+)
+
+const app = createApp(App).use(IonicVue).use(router)
+
 router.isReady().then(() => {
-  app.mount('#app');
-});
+  app.component("font-awesome-icon", FontAwesomeIcon)
+  app.mount("#app")
+})
